33eb14d7896d42744af1fe0f2ab7ccbef82ff77f
Matzoo38 <mslyemi@gmail.com>
2023-06-15 17:28:08 +0200

    dom button okay

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,9 +1,15 @@
+let currentCategory = 0;
+let listWorks=[];
+
+
 async function fetchData() {
 	try {
 	  const response = await fetch('http://localhost:5678/api/works');
 	  const works = await response.json();
 	  console.log(works);
-	  generateWorks(works);
+	  listWorks = works;
+
+	  generateWorks(currentCategory);
 	} catch (error) {
 	  console.error('Une erreur s\'est produite lors de la récupération des works :', error);
 	}
@@ -19,66 +25,55 @@ async function fetchData() {
 	  console.error('Une erreur s\'est produite lors de la récupération des catégories :', error);
 	}
   }
-// let categories = [];
-//   const fetchCategoryData = async () => {
-// 	await fetch('http://localhost:5678/api/categories')
-// 	  .then((res) => res.json().then((cat) => (categories = cat)))
-// 	  .catch((err) => {
-// 		console.log(`Erreur : ${err}`);
-// 	  });
-  
-// 	//Ajout des filtres
-// 	generateCategory(categories);
-//   };
 
-  
-  
-  function generateWorks(works) {
-	const workElement = document.querySelector('.gallery');
+
+
+  function generateWorks(currentCategory) {
 	
-	for (let i = 0; i < works.length; i++) {
-	  const workCreation = document.createElement('figure');
-	  const workCreated = document.createElement('img');
-	  const workName = document.createElement('figcaption');
-  
-	  workCreated.setAttribute('src', works[i].imageUrl);
-	  workName.innerText = works[i].title;
-  
-	  workCreation.appendChild(workCreated);
-	  workCreation.appendChild(workName);
-	  workElement.appendChild(workCreation);
+	const workElement = document.querySelector('.gallery');
+	workElement.innerHTML="";
+	
+	for (let i = 0; i < listWorks.length; i++) { 
+
+		if(listWorks[i].categoryId === currentCategory || currentCategory===0  ){
+			
+			const workCreation = document.createElement('figure');
+			const workCreated = document.createElement('img');
+			const workName = document.createElement('figcaption');
+		
+			workCreated.setAttribute('src', listWorks[i].imageUrl);
+			workName.innerText = listWorks[i].title;
+		
+			workCreation.appendChild(workCreated);
+			workCreation.appendChild(workName);
+			workElement.appendChild(workCreation);
+		}
 	}
   }
   
   function generateCategory(categories) {
 	const categoryElement = document.querySelector('.filter_btn');
-	categoryElement.innerHTML = '';
-  
+
+	const Tous = document.getElementById('Tous');
+    Tous.addEventListener('click', () => {		
+		generateWorks(0);
+	  });
+
+
 	for (let i = 0; i < categories.length; i++) {
+
 	  const categoryButton = document.createElement('button');
+
 	  categoryButton.innerText = categories[i].name;
 	  categoryButton.setAttribute('src',categories[i].id );
-  
+
+	  categoryButton.addEventListener('click', () => {		
+		generateWorks(categories[i].id);
+	  });
 	  
 	  categoryElement.appendChild(categoryButton);
 	}
   }
 
-categoryButton.addEventListener('click', () => {
-		filterWorksByCategory(categories[i].categoryId);
-	  });
-  
-  let currentCategory = 0;
-
-
-
-  function filterWorksByCategory(categoryId) {
-	currentCategory = categoryId;
-	generateCategory();
-  }
-
-  
-  fetchData();
   fetchCategoryData();
-
-  
+  fetchData();
